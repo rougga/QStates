8c6e1f7f78cb968f98fa2862e324f41f694ee3a8
bouga <bouga@DESKTOP-LFQQDTU>
2021-01-25 13:27:40 +0100

    Signed-off-by: ROUGGA <rouggama@gmail.com>
    -v0.5.1
    Signed-off-by:bouga <bouga@DESKTOP-LFQQDTU>

--- src/java/main/CfgHandler.java
+++ src/java/main/CfgHandler.java
@@ -21,7 +21,7 @@ public class CfgHandler {
     
     //MetaData
     public static final String APP = "QStates";
-    public static final String VERSION = "0.5";
+    public static final String VERSION = "0.5.1";
     public static final String COMPANY = "ROUGGA";
     public static final String CLIENT = "";
     //Data files
